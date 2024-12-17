c00de8d7e6d4dc25510536e098f9ab9a2ae65e12
Daniel Figiel <dfigiel10@gmail.com>
2024-12-17 15:56:17 -0500

    initial commit

---
+++ nba-simulator/src/main/java/com/project/nba_simulator/NbaSimulatorBackendApplication.java
@@ -0,0 +1,13 @@
+package com.project.nba_simulator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NbaSimulatorBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NbaSimulatorBackendApplication.class, args);
+	}
+
+}
---
+++ nba-simulator/src/test/java/com/project/nba_simulator/NbaSimulatorBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.nba_simulator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NbaSimulatorBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
